e7d842dda4a382131219d44093dc00cca9c59a94
sachinrao2811 <sachinrao9450@gmail.com>
2022-12-14 17:30:18 +0530

    implement in userInfo

---
+++ src/Components/Constant/EmployeeData.tsx
@@ -0,0 +1,65 @@
+interface EmployeeData {
+    id: number,
+    name: string,
+    dateOfBirth: string,
+    occupation: string,
+    personal: {
+        name: string,
+        address: string,
+        phoneNo: number,
+    },
+    professional: { experiences: string },
+    academic: {
+        collegeName: string,
+        degree: string,
+        passOut: string,
+        address: string,
+    },
+    employment: {
+        currentCompany: string,
+        employmentHistory: [
+            {
+                employer: string,
+                year: number,
+            },
+        ],
+    },
+}
+
+const getDataFromApi = () => {
+    const dataPromise = new Promise((resolve) => {
+        const employee: EmployeeData[] = [
+            {
+                id: 1,
+                name: "John",
+                dateOfBirth: "22/02/2022",
+                occupation: "developer",
+                personal: {
+                    name: "john",
+                    address: "Delhi",
+                    phoneNo: 1234341,
+                },
+                professional: { experiences: "3 years" },
+                academic: {
+                    collegeName: "Sms ",
+                    degree: "B.tech",
+                    passOut: "2022-06-22",
+                    address: "Lucknow",
+                },
+                employment: {
+                    currentCompany: "Celestial",
+                    employmentHistory: [
+                        {
+                            employer: "Google",
+                            year: 2019,
+                        },
+                    ],
+                },
+            },
+        ];
+        resolve(employee);
+    });
+    return dataPromise;
+};
+
+export default getDataFromApi;
